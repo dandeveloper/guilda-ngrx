f2263797a2b7148af7ba8e8ccafacc195f44fb27
Danilo de Carvalho Souza <danilotremere@gmail.com>
2020-03-20 01:34:17 -0300

    feat(todo): starting base todo list component

--- src/app/todo/todo.module.ts
+++ src/app/todo/todo.module.ts
@@ -1,12 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { TodoListComponent } from './todo-list/todo-list.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [TodoListComponent],
+  exports: [
+    TodoListComponent
+  ],
   imports: [
-    CommonModule
+    CommonModule,
+    ReactiveFormsModule,
+    FormsModule,
   ]
 })
 export class TodoModule { }
